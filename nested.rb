6695525d4435f404228b5697900d2942d9ede741
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-27 03:50:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,9 @@ programmer_hash =
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = {{
+:known_for => "Ruby",
+:languages => ["LISP", "C"]}
 
 end
 
