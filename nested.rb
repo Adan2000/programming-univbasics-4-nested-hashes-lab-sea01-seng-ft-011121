2d43f59c54c732b37a1619cdf58c1a1059bbcae7
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-27 03:16:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,11 +52,15 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
+
+
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -66,7 +70,7 @@ def adding_matz
 # }
 # return the entire updated hash
 
-	programmer_hash = 
+programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -82,7 +86,18 @@ def adding_matz
       }
     }
 
+
 end
+
+
+
+
+
+
+
+
+
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -103,8 +118,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper, :alan_kay, :dennis_ritchie] = :yukihiro_matsumoto
 end
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
